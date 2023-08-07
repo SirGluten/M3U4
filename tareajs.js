f94dfe0c178227528504779886cdfb9445e8f5ac
SirGluten <lyallnicolas@gmail.com>
2023-08-07 18:21:15 -0300

    agregados tarea html css y js

---
+++ tareajs.js
@@ -0,0 +1,10 @@
+function changeBg(color){
+    const bg = document.querySelector('body');
+    bg.style.backgroundColor=color;
+    
+}
+
+function llamar(){
+    const mensaje = document.querySelector('body');
+    console.log(mensaje);
+}
